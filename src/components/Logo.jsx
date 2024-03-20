3eb512844952a20d92e3656a34419739ad11714e
Prathamesh Tirlotkar <prathameshtirlotkar2@gmail.com>
2024-03-20 12:47:59 +0530

    Logo updatae

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo() {
   return (
     <span >
-      <img className='w-20 h-12' src="src\components\final-logo.webp" alt="Logo" />
+      <img className='w-20 h-12' src="https://res.cloudinary.com/dmj5vxvtq/image/upload/v1710918995/Appwrite%20Logo/final-logo_gikd73.webp" alt="Logo" />
     </span>                                 
   )
 }
